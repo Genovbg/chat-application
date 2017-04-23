89a234111685e04803cab69febd939569a810d40
Stefan Genov <stefan.genov@mail.bg>
2017-04-23 21:22:54 +0300

    Add files via upload

---
+++ ServerTest.java
@@ -0,0 +1,12 @@
+package StreamSocket;
+
+import javax.swing.JFrame;
+
+public class ServerTest {
+public static void main(String[] args) {
+		
+		Server application = new Server(); // create server
+		application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		application.runServer(); // run server application
+	}
+}
